b950aae47a1ac8c45afa3f21c4dba0a69b3f8a94
DJtedder <31107887+DJtedder@users.noreply.github.com>
2017-10-10 11:45:04 -0400

    ver1

---
+++ resumeOnline/resumeOnline/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace resumeOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "How To Get A Hold Of Me";
+
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Certifications()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+    }
+}
---
+++ resumeOnline/resumeOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="resumeOnline.MvcApplication" Language="C#" %>
